7b8b87777cf08eed28e65a73d3eb3e5ccf4dee95
Adnan Hassan <109860817+imadnanhassan@users.noreply.github.com>
2023-01-26 21:19:18 +0600

    problem solution

---
+++ problem.js
@@ -0,0 +1,36 @@
+// Comparison
+//  > bro
+//  < chato
+//  == soman
+//  !== soman na
+//  >= soman othoba bro
+//  =< soman othoba chato
+
+// ()
+// {} - object, function
+// [] - array
+
+
+
+// 1 problem solution 
+let totalAmount = 1000;
+let oranges = 350;
+let apples = 350;
+
+let foodPrice = oranges + apples;
+let totalTaka = totalAmount - foodPrice
+
+console.log(totalTaka);
+
+
+// 2 problem solution
+
+let mathematics = 75.25;
+let biology = 65;
+let chemistry = 80;
+let physics = 35.45;
+let bangla = 99.50;
+
+let totalSubjectMark = mathematics + biology + chemistry + physics + bangla;
+let totalSubjectResult = totalSubjectMark / 5;
+console.log(totalSubjectResult.toFixed(2));
